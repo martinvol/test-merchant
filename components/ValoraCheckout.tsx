5713811649c1fab09cf33064d7542020fcded54d
Martín Volpe <martin.volpe@gmail.com>
2020-10-06 02:09:55 +0200

    Made QR code load on the fly

--- components/ValoraCheckout.tsx
+++ components/ValoraCheckout.tsx
@@ -3,6 +3,8 @@ import { useShoppingCart } from 'use-shopping-cart'
 import PrintObject from './PrintObject'
 import QRCodeElement from './QRCode'
 import PacmanLoader from "react-spinners/PacmanLoader";
+import BounceLoader from "react-spinners/BounceLoader";
+
 
 
 export function useInterval(callback: () => void, delay: number) {
@@ -32,7 +34,7 @@ const ValoraCheckout = () => {
     formattedTotalPrice,
   } = useShoppingCart()
 
-  const [payment, setPayment] = useState({ status: 'unsubmitted', orderID:'XKCD' })
+  const [payment, setPayment] = useState({ status: 'unsubmitted', orderID:'', qrCOdeRequested:false, address:'', merchantName:"" })
   const [errorMessage, setErrorMessage] = useState('')
   // for now, use our custom API names but possibly keep this all matching to Stripe and within API pings map?
   const PaymentStatus = ({ status }: { status: string }) => {
@@ -65,10 +67,6 @@ const ValoraCheckout = () => {
   }
 
   const checkUpdate = async () => {
-    // TODO replace this with real logic, submit request to webhook to check state of payment
-    const randStatuses : string[] = ['new', 'pending', 'completed', 'expired', 'unresolved']
-    // const randIndex: number = Math.floor(Math.random() * randStatuses.length)
-    // setPayment({ status: randStatuses[randIndex] })
     
     console.log(`Checking update for ${payment.orderID}`)
 
@@ -81,7 +79,6 @@ const ValoraCheckout = () => {
           'Content-Type': 'application/json'
         }
         });
-      
       
       const orderStatus = (await response.json()).status
       if (orderStatus == 'completed'){
@@ -100,37 +97,68 @@ const ValoraCheckout = () => {
     }
   }, 1000)
   
+  
   if (payment['status'] != 'new' && payment['status'] != 'completed'){
     setPayment({...payment, "status": "new"})
   }
 
+  // fetch QR data
+  if (!payment['qrCOdeRequested']){
+    setPayment({...payment, "qrCOdeRequested":true})
+    console.log("starting payment")
+    console.log("Requesting order")
+    
+    fetch('https://py3txmh6a3.execute-api.eu-central-1.amazonaws.com/dev/charges', {
+        method: 'POST',
+        headers: {
+            'X-API-KEY': 'vxKPstUZaI7jSWAtqIaNa9y8htGAfAD4547sUXh9',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"id": "2", "merchantId": "2", "total": totalPrice, "createdAt": "2020-10-05T12:44:00.123Z"})
+    }).then(async response =>  {
+      console.log("this is response")
+      console.log(await response.json())
+      await new Promise(r => setTimeout(r, 2000)); // just for a bit of look and feel
+      // replace with actual data
+      setPayment({...payment, "orderID": "XKCD", "qrCOdeRequested":true, "address":'0xA2C09Ca0a3902ca5e43017159B975c5780cfd4F7', "merchantName":"Seal Sellers Super Sick Symposium"})
+    })
+  }
+     
+    
+    
 
 
+  const qrCodeToShow = 
+      <>
+      {payment['status'] != 'completed' &&
+      <fieldset className="elements-style">
+      <legend>QR Code for Valora</legend>
+      <QRCodeElement
+        merchantAddress={payment.address}
+        merchantName={payment.merchantName}
+        amount={totalPrice}
+        orderID={payment.orderID}
+      />
+      </fieldset>
+    }
+    <PaymentStatus status={payment.status} />
+    </>
+  
   return (
     <div className='valora-checkout'>
-
-      {payment['status'] != 'completed' &&
-        <fieldset className="elements-style">
-        <legend>QR Code for Valora</legend>
-        <QRCodeElement
-          merchantAddress="0xA2C09Ca0a3902ca5e43017159B975c5780cfd4F7"
-          merchantName="Seal Sellers Super Sick Symposium"
-          amount={totalPrice}
-          orderID={payment.orderID}
-        />
-        </fieldset>
-      }
-    {/* <form onSubmit={handleConfirmPayment}>
-      <h3>By confirming, you agree to pay the merchant in your Valora wallet.</h3>
-      <button
-        className="cart-style-background"
-        type="submit"
-        disabled={(payment.status != "unsubmitted")}
-      >
-        Confirm Payment
-      </button>
-    </form> */}
-    <PaymentStatus status={payment.status} />
+      { payment['orderID'] ? 
+      qrCodeToShow
+      : 
+      <>
+      <h2>Loading QrCode</h2>
+      <BounceLoader
+        //css={override}
+        size={50}
+        color={"#123abc"}
+        loading={true}
+      />
+      </>
+    }
     </div>
   )
 }
